8da181df7bdd0bf6fc70804ae976745cd03cecb5
youjin <kujin821@naver.com>
2024-11-28 17:15:54 +0900

    feat : Kakaopay&main병합

--- src/main/java/com/example/redunm/dto/OrderCreateForm.java
+++ src/main/java/com/example/redunm/dto/OrderCreateForm.java
@@ -2,6 +2,7 @@ package com.example.redunm.dto;
 
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,7 +11,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class OrderCreateForm {
-    @NotBlank(message = "상품명은 필수 입력 값입니다.")
+    @NotNull(message = "상품명은 필수 입력 값입니다.")
     private String name;
 
     @Min(value = 1, message = "총 가격은 0보다 큰 값이어야 합니다.")
